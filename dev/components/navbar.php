e3d4517f5d30881a2e94694a835ca6066dda1e66
1Batmain <batduval@gmail.com>
2024-08-07 16:05:49 +0200

    Création de la navbar responsive

--- dev/components/navbar.php
+++ dev/components/navbar.php
@@ -1,11 +1,34 @@
-<nav>
-    <ul>
-        <li><a href="/index.php">Accueil</a></li>
-        <li><a href="/access/login.php">Connexion</a></li>
-        <li><a href="/access/register.php">Inscription</a></li>
-        <li><a href="/pages/abonnement.php">Abonnements</a></li>
-        <li><a href="/pages/shop.php">Boutique</a></li>
-        <li><a href="/pages/blog.php">Blog</a></li>
-        <li><a href="/pages/compte.php">Mon Compte</a></li>
-    </ul>
-</nav>
+     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand text-center " href="/index.php">FitForm</a>
+        <div class="collapse navbar-collapse " id="navbarNav">
+            <ul class="navbar-nav container-fluid mx-5">
+                <li class="nav-item active">
+                    <a class="nav-link" href="./index.php">Accueil <span class="sr-only">(actuel)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/concept.php">Concept</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/abonnements.php">Abonnements</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/clubs.php">Les clubs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/forum.php">Forum</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/boutique.php">Boutique</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./access/inscription.php">Inscription</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./access/connexion.php">Connexion</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
---
+++ dev/components/navbar_connecté.php
@@ -0,0 +1,9 @@
+<nav class='navbar flex-column-md navbar-expand-lg'>
+    <ul>
+        <li><a href="/index.php">FitForm</a></li>
+        <li><a href="/pages/abonnement.php">Abonnements</a></li>
+        <li><a href="/pages/shop.php">Boutique</a></li>
+        <li><a href="/pages/forum.php">Forum</a></li>
+        <li><a href="/pages/compte.php">Mon Compte</a></li>
+    </ul>
+</nav>
