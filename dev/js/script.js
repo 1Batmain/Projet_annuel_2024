179ac5e28dd5ffcfdad6929d9c7db2dadb097f2c
1Batmain <batduval@gmail.com>
2024-10-07 11:41:36 +0200

    cleanage

---
+++ dev/js/script.js
@@ -0,0 +1,56 @@
+// script pour changer le thème (temporaire)
+document.getElementById("theme-toggle").addEventListener("click", function () {
+document.body.classList.toggle("light-theme");
+
+if (document.body.classList.contains("light-theme")) {
+   this.textContent = "🌙";
+} else {
+   this.textContent = "☀️"; 
+}
+});
+
+function openNav(){
+
+  document.getElementById("menuLateral").style.left="0";
+  document.getElementById("masqueFond").style.width="100%";
+  document.getElementById("masqueFond").classList.add("visible");
+}
+
+function closeNav(){
+
+  document.getElementById("menuLateral").style.left="-250px";
+  document.getElementById("masqueFond").style.width="0";
+  document.getElementById("masqueFond").classList.remove("visible");
+
+}
+
+function openModal(type) {
+    // Ferme toutes les modales ouvertes
+    document.querySelectorAll('.modal').forEach(function(modalElement) {
+        var modalInstance = bootstrap.Modal.getInstance(modalElement);
+        if (modalInstance) {
+            modalInstance.hide();
+        }
+    });
+
+    // Retire la classe active des deux boutons
+    document.getElementById('btnConnexion').classList.remove('active');
+    document.getElementById('btnInscription').classList.remove('active');
+
+    var modalElement;
+    if (type === 'connexion') {
+        // Sélectionne la modale de connexion
+        modalElement = document.getElementById('modalConnexion');
+        // Ajouter la classe active au bouton Connexion (pour le surligner)
+        document.getElementById('btnConnexion').classList.add('active');
+    } else if (type === 'inscription') {
+        // Sélectionne la modale d'inscription
+        modalElement = document.getElementById('modalInscription');
+        // Ajoute la classe active au bouton Inscription
+        document.getElementById('btnInscription').classList.add('active');
+    }
+
+    // Ouvrir la modale sélectionnée
+    var modal = new bootstrap.Modal(modalElement);
+    modal.show();
+}
