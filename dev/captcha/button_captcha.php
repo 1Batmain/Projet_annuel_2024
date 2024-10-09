9519c87e1b03e5de082cf0235a0f3279929ebbf5
1Batmain <batduval@gmail.com>
2024-10-09 21:06:11 +0200

    check icon captcha

---
+++ dev/captcha/button_captcha.php
@@ -0,0 +1,5 @@
+<div>
+	<button type="button" class="btn btn-primary my-2 btnCaptcha" onclick = "openModal('captcha')">Verifier le captcha
+	</button>
+	<input type = "hidden" name="result_captcha" class="result_captcha" value="false">
+</div>
